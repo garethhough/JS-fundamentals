4fe1a734fbc8efd512c978bced4c8be36d18eaf1
garethhough <garethhough@googlemail.com>
2022-05-16 09:44:07 +0100

    How to reverse a string

--- week1/notes.js
+++ week1/notes.js
@@ -257,3 +257,36 @@ const coffeeShop = {
 coffeeShop.drinksOrdered("capuccino", "espresso");
 coffeeShop.foodOrdered("cookie", "muffin");
 */
+
+
+//-----How to reverse a string-----
+/*
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+
+reverseString("hello");
+*/
+
+/*
+function reverseString(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    console.log(joinArray);
+}
+reverseString("hello"); 
+*/
